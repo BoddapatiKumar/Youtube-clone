74fd9a7750a62935b50fdd00a6002d00957bba09
BoddapatiKumar <boddapatikumar2002@gmail.com>
2024-12-22 12:47:17 +0530

    Building LiveChat1

---
+++ src/components/LiveChat.js
@@ -0,0 +1,27 @@
+import ChatMessage from "./ChatMessage"
+
+const LiveChat = () => {
+  return (
+    <div className="w-full h-[588px] ml-2 p-2 border border-black bg-gray-100 rounded-lg">
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
+    </div>
+  )
+}
+
+export default LiveChat
