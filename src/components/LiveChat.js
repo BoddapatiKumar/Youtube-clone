4bb0603a09f349e6145233050cf7480b8012e084
BoddapatiKumar <boddapatikumar2002@gmail.com>
2024-12-24 23:36:51 +0530

    ChatSlice

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react"
 import ChatMessage from "./ChatMessage"
 
 const LiveChat = () => {
+  useEffect(()=>{
+    const i=setInterval(() => {
+      //api polling
+    }, 2000);
+
+    return()=>{
+      clearInterval(i);
+    }
+  },[])
   return (
     <div className="w-full h-[588px] ml-2 p-2 border border-black bg-gray-100 rounded-lg">
      <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
