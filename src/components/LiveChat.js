9916e3ab1f764fa0c6521c42bb2695ff0530565b
BoddapatiKumar <boddapatikumar2002@gmail.com>
2024-12-25 17:25:50 +0530

    inputBox in Livechat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,36 +1,51 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import ChatMessage from "./ChatMessage"
+import { useDispatch, useSelector } from "react-redux"
+import { addMessage } from "../Utils/ChatSlice";
+import { generateRandomName } from "../Utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage,setLiveMessage]=useState("");
+  const dispatch=useDispatch();
+
+  const ChatMessages=useSelector((store)=>store.chat.messages);
+
+  //api polling 
   useEffect(()=>{
     const i=setInterval(() => {
       //api polling
+      dispatch(addMessage({
+        name:generateRandomName(),
+        message:"Working on liveChat !",
+      }))
     }, 2000);
 
     return()=>{
       clearInterval(i);
     }
   },[])
+
   return (
-    <div className="w-full h-[588px] ml-2 p-2 border border-black bg-gray-100 rounded-lg">
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-     <ChatMessage name="Kumar" message="Bulding a Live Chat using React!"/>
-    </div>
+    <>
+      <div className="w-full h-[588px] ml-2 p-2 border border-black bg-gray-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+          {ChatMessages.map((c,i)=>(
+          <ChatMessage key={i} name={c.name} message={c.message}/>
+          ))}
+     
+      </div>
+
+      <div className="w-full p-2 ml-2 border border-black">
+        <input 
+          className="w-80 px-2" 
+          value={liveMessage}
+          onChange={(e)=>{
+            setLiveMessage(e.target.value)
+          }}
+          type="text">
+        </input>
+        <button className="px-2 mx-2 bg-green-100 rounded-lg">send</button>
+      </div>
+    </>
   )
 }
 
